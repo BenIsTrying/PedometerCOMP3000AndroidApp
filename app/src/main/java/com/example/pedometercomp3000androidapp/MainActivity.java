db2f5e1c39fc476cc7cb8d656f695a0f16851804
BenIsTrying <91667148+BenIsTrying@users.noreply.github.com>
2024-04-21 14:56:57 +0100

    now can update any feild by clicking the button

--- app/src/main/java/com/example/pedometercomp3000androidapp/MainActivity.java
+++ app/src/main/java/com/example/pedometercomp3000androidapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.pedometercomp3000androidapp;
 
+import static android.app.SearchManager.QUERY;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,6 +14,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -19,13 +28,37 @@ public class MainActivity extends AppCompatActivity {
     ResultSet rs;
     String name,str;
 
+
+    static int getLastDayOfMonthUsingCalendar(int month) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.MONTH, month);
+        return cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         connectionClass = new ConnectionClass();
         connect();
-        //thisnwill get the step data and then display it
+
+        //get day of the month here
+
+
+
+//        SimpleDateFormat sdf = new SimpleDateFormat("'Date\t'dd-MM-yyyy '\t\tand\t\tTime\t'HH:mm:ss z");
+//
+//        String currentDateAndTime = sdf.format(new Date());
+
+//        time.setText(currentDateAndTime);
+
+
+
+
+
+
+
+        //this will get the step data and then display it
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         executorService.execute(() -> {
             try {
@@ -92,10 +125,33 @@ public class MainActivity extends AppCompatActivity {
 
     public void btnClick(View view) {
 
+        int day = 10;
+        //this is where the update function will be used
+
+//this is where the update function will be used
+        ExecutorService executorService2 = Executors.newSingleThreadExecutor();
+        executorService2.execute(() -> {
+            try {
+                con = connectionClass.CONN();
+                String sqlUpdate = "update steps set Step_Day_01 =2000";
+                Statement st = (Statement) con.createStatement();
+                boolean rs = st.execute(sqlUpdate);
+
+
+
+            }catch(Exception e) {
+                Log.e("error", e.getMessage());
+
+            }
+
+
+        });
+
 
 
 
     }
+
     public void connect(){
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         executorService.execute(() ->{
